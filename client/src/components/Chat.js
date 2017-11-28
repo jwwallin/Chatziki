268dc537b584916e3f62c227a04b126af951bf87
jwwallin <jussi.w.wallin@gmail.com>
2017-11-28 18:00:41 +0200

    test rooms

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -4,7 +4,10 @@ import openSocket from 'socket.io-client';
 export default class Chat extends Component {
   componentDidMount() {
     const socket = openSocket('http://localhost:8000');
-    socket.on('success', () => console.log('The socket works!'))
+    socket.on('success', () => console.log('The socket works!'));
+    socket.on('message', (data) => {
+      console.log("incometh data: " + data);
+    });
   }
 
   render() {
--- server/src/server.js
+++ server/src/server.js
@@ -4,11 +4,10 @@ const port = 8000;
 io.listen(port);
 console.log('listening on port ', port);
 
-io.on('connection', (message) => {
+io.on('connection', (socket) => {
     console.log("received connection");
+    socket.join('room');
+    io.emit('success');
+    io.to('room').emit('message', "Yo dawg!");
+});
 
-
-    io.emit('success', {
-        //send username and blah blah
-    });
-  });
