5daebc863cffceb1c318ad03a55dc87b0811fed8
Konsta Purtsi <kovipu@utu.fi>
2017-12-04 18:21:42 +0200

    Chat now reads the visible channel from state

--- client/src/containers/ChatContainer.js
+++ client/src/containers/ChatContainer.js
@@ -2,7 +2,7 @@ import { connect } from 'react-redux';
 import Chat from '../components/Chat';
 
 function mapStateToProps(state, ownProps) {
-  return state.channels.general.messages
+  return state.channels[state.visibleChannel].messages
 }
 
 function mapDispatchToProps(dispatch, ownProps) { return {}; }
